613576c2a7fce7d8b4c5465f6f50a89c346ab848
MichalWadowski <michalpawelwadowski@gmail.com>
2026-03-12 14:42:24 +0100

    Api - projects - fixes responses codes

--- src/modules/project/projects.controller.ts
+++ src/modules/project/projects.controller.ts
@@ -1,11 +1,14 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import {
+  addMember,
   createProject,
+  deleteProject,
   getProjectById,
   listProjects,
   updateProject,
 } from "./projects.service";
 import {
+  AddMemberBody,
   CreateProjectBody,
   ProjectParams,
   UpdateProjectBody,
@@ -58,7 +61,7 @@ export async function getProjectHandler(
 
   try {
     const project = await getProjectById(id, userId);
-    return reply.status(201).send(project);
+    return reply.status(200).send(project);
   } catch (e: any) {
     if (e.statusCode) {
       return reply.status(e.statusCode).send({
@@ -89,6 +92,43 @@ export async function updateProjectHandler(
   }
 }
 
-export function deleteProjectHandler() {}
+export async function deleteProjectHandler(
+  request: FastifyRequest<{ Params: ProjectParams }>,
+  reply: FastifyReply,
+) {
+  const userId = request.user.id;
+  const { id } = request.params;
 
-export function addMemberHandler() {}
+  try {
+    await deleteProject(id, userId);
+    return reply.status(204).send();
+  } catch (e: any) {
+    if (e.statusCode) {
+      return reply.status(e.statusCode).send({
+        message: e.message,
+      });
+    }
+    throw e;
+  }
+}
+
+export async function addMemberHandler(
+  request: FastifyRequest<{ Params: ProjectParams; Body: AddMemberBody }>,
+  reply: FastifyReply,
+) {
+  const { id } = request.params;
+  const userId = request.user.id;
+
+  try {
+    await addMember(id, userId, request.body);
+
+    return reply.status(200).send();
+  } catch (e: any) {
+    if (e.statusCode) {
+      return reply.status(e.statusCode).send({
+        message: e.message,
+      });
+    }
+    throw e;
+  }
+}
