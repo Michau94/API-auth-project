359d5b2ab7acdb83b536b8f1816e4572fb91a3f4
MichalWadowski <michalpawelwadowski@gmail.com>
2026-03-12 14:15:16 +0100

    Api - projects - get project by id controller

--- src/modules/project/projects.controller.ts
+++ src/modules/project/projects.controller.ts
@@ -1,6 +1,10 @@
 import { FastifyReply, FastifyRequest } from "fastify";
-import { createProject, listProjects } from "./projects.service";
-import { CreateProjectBody } from "./projects.types";
+import {
+  createProject,
+  getProjectById,
+  listProjects,
+} from "./projects.service";
+import { CreateProjectBody, ProjectParams } from "./projects.types";
 
 export async function createProjectHandler(
   request: FastifyRequest<{ Body: CreateProjectBody }>,
@@ -21,11 +25,46 @@ export async function createProjectHandler(
   }
 }
 
-export function listProjectsHandler() {}
+export async function listProjectsHandler(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const { id } = request.user;
 
-export function getProjectHandler() {}
+  try {
+    const projects = await listProjects(id);
+    return reply.status(201).send({ data: [...projects] });
+  } catch (e: any) {
+    if (e.statusCode) {
+      return reply.status(e.statusCode).send({
+        message: e.message,
+      });
+    }
+    throw e;
+  }
+}
 
-export function updateProjectHandler() {}
+export async function getProjectHandler(
+  request: FastifyRequest<{ Params: ProjectParams }>,
+  reply: FastifyReply,
+) {
+  const userId = request.user.id;
+  const { id } = request.params;
+
+  try {
+    const project = await getProjectById(id, userId);
+    return reply.status(201).send(project);
+  } catch (e: any) {
+    if (e.statusCode) {
+      return reply.status(e.statusCode).send({
+        message: e.message,
+      });
+    }
+    throw e;
+  }
+}
+
+export async function updateProjectHandler() {}
 
 export function deleteProjectHandler() {}
 
